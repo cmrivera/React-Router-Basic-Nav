316f24aa070fdb9dce4754f531b9d68d15b86320
azrael3069 <cmrivera0004@student.stcc.edu>
2020-03-11 21:24:28 -0400

    route path added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./App.css";
 import { Home, About, Contact, Navigation } from "./components";
 import { Route } from "react-router-dom";
+
+<Route path ="/" component={Home}/>
+<Route path ="/contact" component={Contact}/>
+<Route path ="/about" component={About}/>
 const App = () => (
   <div>
     <Navigation />
