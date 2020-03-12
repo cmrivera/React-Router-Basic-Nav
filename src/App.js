4beb26f49b9a085eb0353787397d914f7299b4d2
azrael3069 <cmrivera0004@student.stcc.edu>
2020-03-11 21:44:45 -0400

    still workin, fun but easy

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import "./App.css";
 import { Home, About, Contact, Navigation } from "./components";
 import { Route } from "react-router-dom";
 
-<Route path ="/" component={Home}/>
-<Route path ="/contact" component={Contact}/>
-<Route path ="/about" component={About}/>
 const App = () => (
   <div>
     <Navigation />
+    <Route path="/" component={Home} />
+    <Route path="/" component={Contact} />
+    <Route path="/" component={About} />
   </div>
 );
 
